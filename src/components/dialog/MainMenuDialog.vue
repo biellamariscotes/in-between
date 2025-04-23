4f7e793eb3f65bb79d20db49c32bb5132b9a2e45
Biella Mariscotes <biella.requina@gmail.com>
2025-04-23 16:05:10 +0800

    style: added style in main menu dialogue

--- src/components/dialog/MainMenuDialog.vue
+++ src/components/dialog/MainMenuDialog.vue
@@ -1,9 +1,34 @@
 <template>
   <el-dialog v-model="mainMenuVisible" title="Warning" width="500" align-center>
-    <h1>Main Menu</h1>
-    <Button variant="secondary">Half</Button>
-    <Button variant="secondary" @click="startNewGame">Quit Game</Button>
-    <button @click="closeMainMenu">Close</button>
+    <el-row justify="end" :gutter="24">
+      <el-col :span="4">
+        <img
+          src="../../assets/img/buttons/ekis.png"
+          alt="back-btn"
+          class="close-btn"
+          @click="closeMainMenu"
+        />
+      </el-col>
+    </el-row>
+
+    <el-row justify="center" :gutter="24">
+      <el-col :span="26"><p class="title-main-menu">Main Menu</p></el-col>
+    </el-row>
+
+    <div class="menu-btn-cont">
+      <img
+        src="../../assets/img/buttons/main-menu/how-to-play.png"
+        alt="deal-now-btn"
+        class="main-menu-btns"
+      />
+      <img
+        src="../../assets/img/buttons/main-menu/restart-game.png"
+        alt="how-to-play-btn"
+        class="main-menu-btns"
+        @click="startNewGame"
+      />
+    </div>
+
     <template #footer>
       <div class="dialog-footer"></div>
     </template>
@@ -36,3 +61,34 @@ watch(mainMenuVisible, (newValue) => {
   eventBus.emit('untoggle-main-menu', newValue)
 })
 </script>
+
+<style scoped>
+.main-menu-btns {
+  width: 70%;
+  cursor: pointer;
+  padding-bottom: 16px;
+}
+
+.title-main-menu {
+  margin-bottom: 0;
+  font-size: 4rem;
+  padding-bottom: 0.5em;
+  color: #000;
+}
+
+.close-btn {
+  padding-top: 1em;
+  width: 40%;
+  cursor: pointer;
+}
+
+.menu-btn-cont {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.el-dialog__header.show-close {
+  padding-right: 0;
+}
+</style>
